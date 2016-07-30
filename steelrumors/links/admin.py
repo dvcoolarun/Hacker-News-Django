cbd207fa1ad74c2cb68999a14b4f109a1aa05525
aruntakkar <aruntakkar5@gmail.com>
2016-07-30 19:49:41 +0530

    user and user-registration with admin side votes

--- steelrumors/links/admin.py
+++ steelrumors/links/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Link, Vote, UserProfile
+
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
 # Register your models here.
